f8afa0428250526a8b292ec260b492410fee6be5
Chris Schuhmacher <chris@fandom.com>
2019-11-06 09:00:27 -0800

    finishing with tuples

--- tuples.ts
+++ tuples.ts
@@ -14,3 +14,10 @@ const pepsi: Drink = ['brown', true, 40];
 pepsi[0] = 40; // TS throws error because property at index 0 must be a string
 
 // Why tuples and when to use
+
+const carSpecs: [number, number] = [400, 3354]; // confusing
+
+const carStats = {
+    horsepower: 400,
+    weight: 3354
+};
