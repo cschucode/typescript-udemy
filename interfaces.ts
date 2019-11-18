1deda5ccee37e94ca494a920b1695dafd4561956
Chris Schuhmacher <chris@fandom.com>
2019-11-18 09:07:19 -0800

    Added comments on interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,7 +1,9 @@
+// An TS interface describing object with a summary method that returns a string
 interface Reportable {
     summary(): string;
 }
 
+// car object that matches Reportable interface
 const oldCivic = {
     name: 'civic',
     year: new Date(),
@@ -11,6 +13,8 @@ const oldCivic = {
     },
 };
 
+// Because we made our interface generic, we can have a drink object
+// that also has a summary method that returns a string
 const drink = {
     color: 'brown',
     carbonated: true,
@@ -20,9 +24,12 @@ const drink = {
     }
 };
 
+// A function that uses our Reportable interface
 const printSummary = (item: Reportable): void => {
     console.log(item.summary());
 };
 
+// Calling the function and because both objects satisfy the 
+// Reportable interface, no errors are shown
 printSummary (oldCivic);
 printSummary(drink);
